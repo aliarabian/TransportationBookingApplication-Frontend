f18aec200e0ae71415fc673aee082dffa096a762
Ali Arabian <ali@qulu.faza>
2022-06-04 14:56:08 +0430

    Adds Expansion/Collapse on flight cards using angular animation

--- src/app/flights-list/flights-list.component.ts
+++ src/app/flights-list/flights-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Flight} from "./flight";
 import {HttpClient} from "@angular/common/http";
 import {SeatingSection} from "./seating-section";
@@ -6,31 +6,33 @@ import {FlightCardModalComponent} from "../flight-card-modal/flight-card-modal.c
 import {MatDialog} from "@angular/material/dialog";
 import {animate, AUTO_STYLE, state, style, transition, trigger} from "@angular/animations";
 
-const DEFAULT_DURATION = 300;
-
 @Component({
   selector: 'app-flights-list',
   templateUrl: './flights-list.component.html',
   styleUrls: ['./flights-list.component.css'],
   animations: [
     trigger('collapsable', [
-      state('false', style({height: AUTO_STYLE, visibility: AUTO_STYLE})),
-      state('true', style({height: '0', visibility: 'hidden'})),
-      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
-      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+      state('false', style({
+        height: 0, width: 0, visibility: 'hidden', opacity: 0, margin:'-16px'
+      })),
+      state('true', style({
+        height: AUTO_STYLE, visibility: AUTO_STYLE, opacity: 1
+      })),
+      transition('false => true', animate(1000 + 'ms ease-in-out')),
+      transition('true => false', animate(1000 + 'ms ease-in-out'))
     ])
-  ]
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FlightsListComponent implements OnInit {
+export class FlightsListComponent implements OnInit, OnChanges {
 
   @Input() flights?: Flight[];
-  collapse: boolean[][] = [[]];
+  collapsed: boolean[][] = [];
 
   constructor(private http: HttpClient, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
-
   }
 
   openModal(flight: Flight, section: SeatingSection) {
@@ -40,5 +42,23 @@ export class FlightsListComponent implements OnInit {
         section: section
       }
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.flights.isFirstChange()) {
+      for (let i = 0; i < changes.flights.currentValue.length; i++) {
+        let sectionCard: boolean[] = []
+        for (let j = 0; j < changes.flights.currentValue[i].sections.length; j++) {
+          sectionCard.push(false);
+        }
+        this.collapsed.push(sectionCard);
+      }
+    }
+  }
+
+
+  onCollapse($event: MouseEvent, i: number, j: number) {
+    $event.stopImmediatePropagation();
+    this.collapsed[i][j] = !this.collapsed[i][j];
   }
 }
